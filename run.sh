8d67ea478218c625ed026785ad50568ecfc16fc3
Mark Glasgow <markglasgow@gmail.com>
2020-09-08 10:42:37 +0100

    shaking dependency tree bugs

--- run.sh
+++ run.sh
@@ -3,12 +3,14 @@ export setenv DEFAULT_ADMIN_PASSWORD=passwordpassword
 export setenv MPWD=Kaplan_12
 #!/bin/zsh
 
-export 'jwtSecret = aba318899cb6a81ff9698733c21d5e61406964272c5b7efe1fdfc55db410bf35' >> ~/.zshenv
-export 'DEFAULT_ADMIN_PASSWORD=passwordpassword' >> ~/.zshenv
-export 'MPWD=Kaplan_12' >> ~/.zshenv
-echo $jwtSecret
-echo $DEFAULT_ADMIN_PASSWORD
-echo $MPWD
+#export 'jwtSecret = aba318899cb6a81ff9698733c21d5e61406964272c5b7efe1fdfc55db410bf35' >> ~/.zshenv
+#export 'DEFAULT_ADMIN_PASSWORD=passwordpassword' >> ~/.zshenv
+#export 'MPWD=Kaplan_12' >> ~/.zshenv
+echo ""
+echo "Environmental variables:"
+echo "jwtSecret=" $jwtSecret
+echo "DEFAULT_ADMIN_PASSWORD=" $DEFAULT_ADMIN_PASSWORD
+echo "MPWD=" $MPWD
 cd eleos
 #node src/tasks/createAdminUser.js
 #check for env here
