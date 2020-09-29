862310d7fa59a29a7aadf2e861ac66f6ae7f110e
Mark Glasgow <markglasgow@gmail.com>
2020-09-29 17:39:40 +0100

    auth/dashboard

--- eleos-api/src/auth/auth.controller.js
+++ eleos-api/src/auth/auth.controller.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable comma-dangle */
 /* eslint-disable camelcase */
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const FormData = require('form-data');
-const fetch = require('node-fetch');
-const pool = require('../db/db');
-require('dotenv').config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+const pool = require("../db/db");
+require("dotenv").config();
 
 // Customer registration
 
 const get = (req, res) => {
   res.json({
-    message: 'Hello Auth 🔐',
+    message: "Hello Auth 🔐",
   });
 };
 
-const createTokenSendResponse = ({ user_id, role = 'customer' }, res, next) => {
+const createTokenSendResponse = ({ user_id, role = "customer" }, res, next) => {
   const payload = {
     user: user_id,
     role,
@@ -25,12 +25,12 @@ const createTokenSendResponse = ({ user_id, role = 'customer' }, res, next) => {
     payload,
     process.env.jwtSecret,
     {
-      expiresIn: '12h',
+      expiresIn: "12h",
     },
     (err, token) => {
       if (err) {
         res.status(422);
-        const error = new Error('Unable to login');
+        const error = new Error("Unable to login");
         next(error);
       } else {
         res.json({ token, user_id });
@@ -39,6 +39,116 @@ const createTokenSendResponse = ({ user_id, role = 'customer' }, res, next) => {
   );
 };
 
+/**
+ * 
+ */
+const ecLogin = async (req, res, next) => {
+  try {
+    const {
+      name,
+      subdomain,
+      target_config_id,
+      admin_email,
+      admin_password,
+      admin_firstname,
+      admin_lastname,
+      admin_phone,
+      active,
+    } = req.body;
+
+    // prepare data to initialise odoo instance
+
+    const fd = new FormData();
+
+    fd.append("master_pwd", process.env.MPWD);
+    fd.append("name", name);
+    fd.append("login", admin_email);
+    fd.append("password", admin_password);
+    fd.append("phone", admin_phone);
+    fd.append("lang", "en_GB");
+    fd.append("country_code", "gb");
+    /*
+     *
+     * TEST DB : ec2-3-10-212-243
+     * DEV DB : ec2-35-178-199-156
+     * */
+    const response = await fetch(
+      "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web/login",
+      {
+        method: "POST",
+        body: fd,
+        headers: fd.getHeaders(),
+      }
+    );
+    var res = null;
+    var csrfToken = null;
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function (aEvt) {
+      if (req.readyState == 4) { // 4 == Done
+        console.log("test1");
+        var csrfToken = req.getResponseHeader("x-csrf-token");
+        console.log(csrfToken);
+
+        if (req.status == 200) {
+          console.log("test2");
+          document.write(req.responseText);
+          //window.location.href = "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com";
+          //res.setRequestHeader("x-csrf-token", "fetch");
+        } else alert("Error contacting Odoo back-end.\n");
+      }
+      console.log("test3");
+      req.open("POST","http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web/login", false);
+      req.setRequestHeader("x-csrf-token","e24b4112a0cbff1ccf02b182f705e6971d035507o");
+      req.setRequestHeader("Content-Type","application/json; charset=utf-8");
+      req.setRequestHeader("Accept", "text/plain");
+      req.setRequestHeader("X-Odoo-dbfilter", "data");
+      if (req.readyState === 4) {
+        console.log("test4");
+        req = JSON.parse(this.responseText);
+      }
+      
+      //req.send(JSON.stringify(body));
+    };
+    req.open("GET","http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web/login/",true); //true means request will be async
+    req.send(); 
+
+
+    
+
+    // encrypt password for STA database
+
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(admin_password, salt);
+
+    // write into STA database
+
+    const newUser = await pool.query(
+      "INSERT INTO users (name, subdomain, target_config_id, admin_email, admin_password, admin_firstname, admin_lastname, active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+      [
+        name,
+        subdomain,
+        target_config_id,
+        admin_email,
+        bcryptPassword,
+        admin_firstname,
+        admin_lastname,
+        active,
+      ]
+    );
+
+    createTokenSendResponse(newUser.rows[0], res, next);
+  } catch (err) {
+    res.status(500);
+    next(err);
+  }
+};
+
+/**
+ * Register
+ */
 const register = async (req, res, next) => {
   try {
     const {
@@ -57,22 +167,22 @@ const register = async (req, res, next) => {
 
     const fd = new FormData();
 
-    fd.append('master_pwd', process.env.MPWD);
-    fd.append('name', name);
-    fd.append('login', admin_email);
-    fd.append('password', admin_password);
-    fd.append('phone', admin_phone);
-    fd.append('lang', 'en_GB');
-    fd.append('country_code', 'gb');
-      /*
-      *
-      * TEST DB : ec2-3-10-212-243
-      * DEV DB : ec2-35-178-199-156
-      * */ 
+    fd.append("master_pwd", process.env.MPWD);
+    fd.append("name", name);
+    fd.append("login", admin_email);
+    fd.append("password", admin_password);
+    fd.append("phone", admin_phone);
+    fd.append("lang", "en_GB");
+    fd.append("country_code", "gb");
+    /*
+     *
+     * TEST DB : ec2-3-10-212-243
+     * DEV DB : ec2-35-178-199-156
+     * */
     const response = await fetch(
-      'http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web/database/create',
+      "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web/database/create",
       {
-        method: 'POST',
+        method: "POST",
         body: fd,
         headers: fd.getHeaders(),
       }
@@ -88,7 +198,7 @@ const register = async (req, res, next) => {
     // write into STA database
 
     const newUser = await pool.query(
-      'INSERT INTO users (name, subdomain, target_config_id, admin_email, admin_password, admin_firstname, admin_lastname, active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+      "INSERT INTO users (name, subdomain, target_config_id, admin_email, admin_password, admin_firstname, admin_lastname, active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
       [
         name,
         subdomain,
@@ -121,7 +231,7 @@ const login = async (req, res, next) => {
       createTokenSendResponse(req.foundUser, res, next);
     } else {
       res.status(422);
-      throw new Error('Unable to Login');
+      throw new Error("Unable to Login");
     }
   } catch (error) {
     res.status(res.statusCode === 200 ? 500 : res.statusCode);
@@ -140,7 +250,7 @@ const adminLogin = async (req, res, next) => {
       createTokenSendResponse(req.foundUser, res, next);
     } else {
       res.status(422);
-      throw new Error('Unable to login');
+      throw new Error("Unable to login");
     }
   } catch (error) {
     res.status(res.statusCode === 200 ? 500 : res.statusCode);
@@ -150,14 +260,14 @@ const adminLogin = async (req, res, next) => {
 
 const isVerify = async (req, res, next) => {
   try {
-    if (req.user.role && req.user.role === 'admin') {
+    if (req.user.role && req.user.role === "admin") {
       res.json({ verified: true, admin: true });
     } else {
       res.json({ verified: true, admin: false });
     }
   } catch (error) {
     res.status(500);
-    next(new Error('Server Error'));
+    next(new Error("Server Error"));
   }
 };
 
--- eleos-static/src/components/customer/Dashboard.js
+++ eleos-static/src/components/customer/Dashboard.js
@@ -6,7 +6,7 @@ import EditUserDetails from "./EditUserDetails";
 import Grid from "@material-ui/core/Grid";
 import UserDetails from "./UserDetails";
 import { useStyles } from "../../styles/DashboardStyles";
-import { get as getCookie} from 'browser-cookies'
+import { get as getCookie } from "browser-cookies";
 
 const Dashboard = ({ setAuth }) => {
   const classes = useStyles();
@@ -18,20 +18,15 @@ const Dashboard = ({ setAuth }) => {
     admin_lastname: "",
     active: "",
   });
-  const db = useState({
-    subdomain: ""
-  });
+  const db = "data";
 
   const body = useState({
- 
     admin_email: "",
-    admin_password: ""
+    admin_password: "",
   });
-  
+
   //const body = { admin_email, admin_password };
   const [isEditing, setIsEditing] = useState(false);
-  
-
 
   async function getUserDetails() {
     try {
@@ -50,79 +45,39 @@ const Dashboard = ({ setAuth }) => {
     }
   }
 
-
   /**
-   * 
+   *
    * getToken retrieves the CSRF token from EC2
-   * 
+   *
    */
   async function getToken() {
     try {
-      return fetch("http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web?db=" + db, { // Added the trailing slash here 
-        method: 'POST',
-        credentials: "same-origin", // Added this line
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFToken': getCookie('csrftoken'), // getting token from cookies
-        },
-        body: JSON.stringify(body),
-        
-      }).then(response => {
-        return response.json();
-      });
-     
-
-      //const parseRes = await response.json();
-      
-
-      /**var res = null;
-      var tryout = new XMLHttpRequest();
-      tryout.open(
-        "GET",
-        "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web?db=" +
-          db,
-        false
-      );
-      tryout.withCredentials = true;
-      tryout.setRequestHeader(
-        "x-csrf-token",
-        "e24b4112a0cbff1ccf02b182f705e6971d035507o"
-      );
-      tryout.setRequestHeader("Accept", "application/json");
-      tryout.setRequestHeader(
-        "Content-Type",
-        "application/json; charset=utf-8"
-      );
-      tryout.send(null);
-      if (tryout.readyState === 4) {
-       // var csrfToken = tryout.getResponseHeader("x-csrf-token");
-
-        tryout.open(
-          "POST",
-          "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web?db=" +
-            db,
-          false
-        );
-        tryout.setRequestHeader("x-csrf-token", 'fetch');
-        tryout.setRequestHeader(
-          "Content-Type",
-          "application/json; charset=utf-8"
-        );
-        tryout.setRequestHeader("Accept", "application/json");
-        console.log("test");
-        console.log('fetch');
-        //tryout.send(JSON.stringify(body));
-
-        if (tryout.readyState === 4) {
-          res = JSON.parse(this.responseText);
+      try {
+        setLoading(true);
+  
+        const response = await fetch('http://eleos-api.eba-rfdhwwp4.eu-west-2.elasticbeanstalk.com/auth/ecLogin', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(inputs),
+        });
+  
+        const parseRes = await response.json();
+  
+        if (parseRes.token) {
+          localStorage.setItem('token', parseRes.token);
+          setAuth(true);
+          toast.success('Registered Successfully');
+          setLoading(false);
+        } else {
+          setLoading(false);
+          setAuth(false);
+          toast.error(parseRes);
         }
-        window.location.href =
-          "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com";
+      } catch (err) {
+        toast.error(err.message);
       }
-      */
-    } catch (err) {
-      console.error(err.message);
-    }
+    
+   
   }
 
   const toggleEdit = () => {
@@ -184,7 +139,7 @@ const Dashboard = ({ setAuth }) => {
 
 export default Dashboard;
 
-      /**  https://html.developreference.com/article/10364287/Swift+Regex+to+retrieve+token+from+a+page+html
+/**  https://html.developreference.com/article/10364287/Swift+Regex+to+retrieve+token+from+a+page+html
       let regex = try NSRegularExpression(pattern: "\"csrf_token\":([A-z0-9]*")
 
       let a = text!.matchingStrings(regex: "\"csrf_token\":([A-z0-9]*")
@@ -203,7 +158,7 @@ export default Dashboard;
           : ""
       }*/
 
-       /**
+/**
        * 
        
       const response = await fetch(
@@ -215,4 +170,118 @@ export default Dashboard;
       );
       //setUserDetails(parseRes);
       //var csrfToken = parseRes.querySelector("#csrf_token").value;
+      */
+
+/*
+      return fetch("http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web?db=" + db, { // Added the trailing slash here 
+        method: 'POST',
+        credentials: "same-origin", // Added this line
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': getCookie('csrftoken'), // getting token from cookies
+        },
+        body: JSON.stringify(body),
+        
+      }).then(response => {
+        return response.json();
+      });
+      */
+  //var tryout = new XMLHttpRequest();
+
+      // Get CSRF
+      /** 
+      tryout.open(
+        "GET",
+        "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com?db=" + "data"
+      ); //get csrf
+      tryout.withCredentials = true;
+      tryout.setRequestHeader("x-csrf-token", "fetch");
+      tryout.setRequestHeader("Accept", "application/json");
+      tryout.setRequestHeader(
+        "Content-Type",
+        "application/json; charset=utf-8"
+      );
+      tryout.send(null);
+
+      if (tryout.readyState === 4) {
+        var csrfToken = tryout.getResponseHeader("x-csrf-token");
+
+        tryout.open(
+          "POST",
+          "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web/login/",
+          false
+        );
+        tryout.setRequestHeader("x-csrf-token", csrfToken);
+        tryout.setRequestHeader(
+          "Content-Type",
+          "application/json; charset=utf-8"
+        );
+        tryout.setRequestHeader("Accept", "application/json");
+        req.setRequestHeader("X-Odoo-dbfilter", "data");
+
+        if (req.status == 200) {
+          console.log("test2");
+          document.write(req.responseText);
+          window.location.href =
+            "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com";
+          //res.setRequestHeader("x-csrf-token", "fetch");
+        } else alert("Error contacting Odoo back-end.\n");
+      }
+
+      //tryout.send(JSON.stringify(obj));
+      tryout.send("data");
+
+      if (tryout.readyState === 4) {
+        res = JSON.parse(this.responseText);
+      }
+
+      */
+     
+
+      //const parseRes = await response.json();
+
+      /**var res = null;
+      var tryout = new XMLHttpRequest();
+      tryout.open(
+        "GET",
+        "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web?db=" +
+          db,
+        false
+      );
+      tryout.withCredentials = true;
+      tryout.setRequestHeader(
+        "x-csrf-token",
+        "e24b4112a0cbff1ccf02b182f705e6971d035507o"
+      );
+      tryout.setRequestHeader("Accept", "application/json");
+      tryout.setRequestHeader(
+        "Content-Type",
+        "application/json; charset=utf-8"
+      );
+      tryout.send(null);
+      if (tryout.readyState === 4) {
+       // var csrfToken = tryout.getResponseHeader("x-csrf-token");
+
+        tryout.open(
+          "POST",
+          "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com/web?db=" +
+            db,
+          false
+        );
+        tryout.setRequestHeader("x-csrf-token", 'fetch');
+        tryout.setRequestHeader(
+          "Content-Type",
+          "application/json; charset=utf-8"
+        );
+        tryout.setRequestHeader("Accept", "application/json");
+        console.log("test");
+        console.log('fetch');
+        //tryout.send(JSON.stringify(body));
+
+        if (tryout.readyState === 4) {
+          res = JSON.parse(this.responseText);
+        }
+        window.location.href =
+          "http://ec2-35-178-199-156.eu-west-2.compute.amazonaws.com";
+      }
       */
